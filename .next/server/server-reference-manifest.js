da9ca0b06ae238b859e3c8b592e332b6f7fb50d6
Parambir Singh <paramstr@gmail.com>
2024-10-06 23:18:08 +1300

    Added editability to system prompt and fixed sizing.

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"8COK5sykJhRB7yM5FI83aJKos05OBWO5LKkIzQ7wUFc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KvkJgUhDfdSgwg3JNDyS1i2uRVQ/tl+s0ibwfdA70RA=\"\n}"
